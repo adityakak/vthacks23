334aafdb2d33881ced75f4c5998d7f381e49c860
Harshal Arakala <harshal.arakala@gmail.com>
2023-09-16 02:22:30 -0400

    Added description

--- frontend/VTHacks23/src/App.tsx
+++ frontend/VTHacks23/src/App.tsx
@@ -10,4 +10,12 @@ function App() {
   )
 }
 
+/* Description:
+
+-) __(Insert Name) provides a percent score for both the Educational Equity and the Environmental footprint of the address given
+-) A best alternative house to the one that was searched is displayed along with many others nearby and their respective ratings
+-) Pay attention to the averages in your area!
+-) Click the listings to view them on Homes.com
+
+*/
 export default App
