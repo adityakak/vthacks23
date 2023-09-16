c7445256abe0c9c5a11684f2b1a7d7cf178b6545
HariPeri <hariperi2009@gmail.com>
2023-09-16 14:45:32 -0400

    Home Page improvements

--- frontend/VTHacks23/src/components/Form.tsx
+++ frontend/VTHacks23/src/components/Form.tsx
@@ -19,9 +19,18 @@ function Form() {
   }
   return (
     <>
-      <div className="relative pb-24 bg-home-bg bg-cover bg-no-repeat h-[41rem] z-[-50] bg-red-800 ">
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-[-5]"style={{ pointerEvents: 'none' }} />
-        
+     <div className="relative pb-24 bg-home-bg bg-cover bg-no-repeat h-[41rem] z-[10] bg-red-800">
+  <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-[2]" />
+
+  {/* Centered orange rectangle */}
+  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-7xl border-4  border-orange-500 w-[40rem] h-80 flex flex-col justify-center items-center rounded-3xl z-20" >
+    <div className="absolute top-[2%] left-[1%] border-white border-4 h-[96%] w-[98%] z-[20] rounded-2xl" />
+      Advising Homes for dayz
+    </div>
+    
+</div>
+      <div className="flex justify-center my-10 text-white text-4xl italic">
+        Enter your address below
       </div>
       <form
           className="relative flex flex-col justify-center items-center h-full py-24"
@@ -73,7 +82,7 @@ function Form() {
             </div>
             <div>
               <select
-                className="mb-2 w-full rounded-lg bg-blue-400 px-5 py-3 placeholder-white text-white mx-auto"
+                className="mb-2 w-full rounded-lg bg-blue-400 px-5 py-3 placeholder-white text-white mx-auto border-r-8 border-transparent"
                 value={state}
                 placeholder="State"
                 {...register("state", {
