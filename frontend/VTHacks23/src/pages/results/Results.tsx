a9b8614aa6d6e9fca3aa548678cfd33c475ec81d
HariPeri <hariperi2009@gmail.com>
2023-09-16 19:21:02 -0400

    Pie Chart Changes

--- frontend/VTHacks23/src/pages/results/Results.tsx
+++ frontend/VTHacks23/src/pages/results/Results.tsx
@@ -2,29 +2,27 @@ import Navbar from "../../components/Navbar";
 import Overall from "./overallPie";
 
 const chartStyles = {
-  // Apply your styles here, for example:
-  width: '300px',
-  height: '300px',
+    // Apply your styles here, for example:
+    width: "300px",
+    height: "300px",
 };
 
 function Results() {
-  return (
-    <div>
-      <Navbar />
-      <div id="dashboard">
-        <div
-          id="overallPieDiv"
-          className="flex justify-left p-[30px] h-[500px] relative"
-        >
-          <Overall />
-          <div className="mt-10 relative group">
-            <span className="text-white text-4xl italic">Your Overall Rating</span>
-            <span className="absolute left-0 w-full h-1 bg-orange-400 transition-all duration-500 opacity-0 group-hover:opacity-100"></span>
-          </div>
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <div className="md:flex justify-left p-[30px] min-w-[500px] relative">
+                    <Overall />
+                    <div className="mt-10 relative group">
+                        <span className="text-white text-4xl bg-left-bottom bg-gradient-to-r from-orange-400 to-orange-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out italic mt-10">
+                            Your Overall Rating
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Results;
--- frontend/VTHacks23/src/pages/results/overallPie.tsx
+++ frontend/VTHacks23/src/pages/results/overallPie.tsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import { Chart, CategoryScale, ArcElement } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import React from "react";
+import { Chart, CategoryScale, ArcElement } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 Chart.register(CategoryScale, ArcElement);
 const overallPie: React.FC = () => {
-  const data = {
-    labels: ['A', 'B', 'C'],
-    datasets: [
-      {
-        data: [30, 40, 10,20],
-        backgroundColor: ['#FF5733', '#33FF57', '#5733FF','#000000'],
-      },
-    ],
-  };
+    const data = {
+        labels: ["A", "B", "C"],
+        datasets: [
+            {
+                data: [30, 40, 10, 20],
+                backgroundColor: ["#FF5733", "#33FF57", "#5733FF", "#000000"],
+                borderWidth: [0, 0, 0, 0],
+            },
+        ],
+    };
 
-  return (
-    <div>
-      <Doughnut data={data}
-      options={{
-        plugins: {
-          legend: {
-            display: false, // Hide the legend
-          },
-        },
-      }}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Doughnut
+                data={data}
+                options={{
+                    plugins: {
+                        legend: {
+                            display: false, // Hide the legend
+                        },
+                    },
+                }}
+            />
+        </div>
+    );
 };
 
 export default overallPie;
