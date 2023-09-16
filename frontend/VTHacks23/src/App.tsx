efdb5425f46aa47a93ce1db7ed6de447e58fd4f0
Harshal Arakala <harshal.arakala@gmail.com>
2023-09-16 02:44:44 -0400

    added routing

--- frontend/VTHacks23/src/App.tsx
+++ frontend/VTHacks23/src/App.tsx
@@ -1,11 +1,17 @@
+import { Route, Router, Routes } from "react-router";
 import Form from "./components/Form";
 
 function App() {
 
   return (
-    <>
-    <Form/>
-    </>
+      <Router>
+        <div>
+          <Routes>
+            <Route path = "/"  element = {<Home />}/>
+            <Route path = "/results" element = {<Results />}/>
+          </Routes>
+        </div>
+      </Router>
   )
 }
 
