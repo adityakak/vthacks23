6d43f3be664df9987862eeaecb37cf116c21794c
Harshal Arakala <harshal.arakala@gmail.com>
2023-09-16 03:55:41 -0400

    backdrop changes

--- frontend/VTHacks23/src/components/Home.tsx
+++ frontend/VTHacks23/src/components/Home.tsx
@@ -1,17 +1,20 @@
 import Form from "./Form"
 function Home(){
   return (
-    <Form/>
+    <div className="page">
+        <div id= "head" className="header bg-orange-500">
+            <image href = ""> </image>
+        </div>
+        <div id="body" className="backdrop-blur-sm md:backdrop-blur-lg bg-[/Users/harakala/Code/vthacks23/frontend/VTHacks23/src/img/backdrop.jpg]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div id="foot">
+
+        </div>
+        <Form/>
+    </div>
+    
   )
 }
-
-/* Description:
-
--) __(Insert Name) provides a percent score for both the Educational Equity and the Environmental footprint of the address given
--) A best alternative house to the one that was searched is displayed along with many others nearby and their respective ratings
--) Pay attention to the averages in your area!
--) Click the listings to view them on Homes.com
-
-*/
 
 export default Home
