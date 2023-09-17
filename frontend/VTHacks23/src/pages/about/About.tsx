7ff88351ac026a37442221b5befb6ff5b68051e8
Prabhath Tangella <prabhathtangella04@gmail.com>
2023-09-17 07:59:11 -0400

    commit

--- frontend/VTHacks23/src/pages/about/About.tsx
+++ frontend/VTHacks23/src/pages/about/About.tsx
@@ -21,8 +21,19 @@ function About() {
                     />
                 ))}
             </div>
+            <div className="my-10">
+                <Divider />
+            </div>
+            <div className="text-white text-4xl leading-relaxed w-3/4 mx-auto my-10">
+                Description:
 
-            <Divider />
+                ScoreMyHome provides a percent score for both the Educational Equity and the Environmental footprint of the address given
+                 A best alternative house to the one that was searched is displayed along with many others nearby and their respective ratings
+                Pay attention to the averages in your area!
+                Click the listings to view them on Homes.com
+            </div>
+            
+
             <Footer />
         </div>
     );
